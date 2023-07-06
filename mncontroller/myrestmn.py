e5bb6c0f7bc066e8694d2e1c026634eb0abdbe7b
ax1an9 <cp3_hyx@foxmail.com>
2023-07-06 17:15:11 +0800

    增加注释

--- mncontroller/myrestmn.py
+++ mncontroller/myrestmn.py
@@ -51,47 +51,46 @@ class MininetRest(Bottle):
                     "cpuLimit": req['cpu'],
                     'mem':req['mem']
                     })
-            #     uuid:{
-            #         'id':uuid,
-            #         'type':"host",
-            #         "cpuLimit": req['cpu'],
-            #         'mem':req['mem']
-            #         }
-            # })
-            # updatehosts({
-            #     uuid:{
-            #         'id':uuid,
-            #         'type':"host",
-            #         "cpuLimit": req['cpu'],
-            #         'mem':req['mem']
-            #         }
-            # })
         return {"code":200,"info":"success" if condition else "fail to add"}
-    
-    def add_link(self, node_name):
-        h1 = self.net.addHost(node_name)
-        return {'nodes': [n for n in self.net]}
 
     def get_nodes(self):
+        """
+        获取所有node的信息
+        """
         print({'nodes': [n for n in self.net]})
         return {'nodes': [n for n in self.net]}
 
     def get_node(self, node_name):
+        """
+        获取指定node的信息
+        """
         node = self.net[node_name]
         return {'intfs': [i.name for i in node.intfList()], 'params': node.params}
 
     def post_node(self, node_name):
+        """
+        给Node添加额外的信息
+        """
         node = self.net[node_name]
         node.params.update(request.json['params'])
         return {"code":200,"info":"success"}
 
     def get_hosts(self):
+        """
+        获取host列表
+        """
         return {'hosts': [h.name for h in self.net.hosts]}
 
     def get_switches(self):
+        """
+        获取switch列表
+        """
         return {'switches': [s.name for s in self.net.switches]}
 
     def get_links(self):
+        """
+        获取所有现存链路列表
+        """
         return {'nodes': [n for n in self.net],
                 'links': [dict(name=l.intf1.node.name + '-' + l.intf2.node.name,
                                node1=l.intf1.node.name, node2=l.intf2.node.name,
